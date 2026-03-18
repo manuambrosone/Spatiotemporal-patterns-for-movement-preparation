7ca49eb43fa3174df0ece94297a9da1f28c0eec7
manuambrosone <manuel.ambrosone@gmail.com>
2026-03-18 13:24:40 +0100

    Add files via upload

---
+++ tensor_convolve.m
@@ -0,0 +1,20 @@
+function X = tensor_convolve(W,H)
+%from helper.reconstruct function in seqNMF toolbox
+%reported from MacDowell & Buschman., 2020
+
+[N,K,L] = size(W);
+[~,T] = size(H);
+
+% zeropad by L
+H = [zeros(K,L),H,zeros(K,L)];
+T = T+2*L;
+X = zeros(N,T);
+
+for tau = 1:L 
+    X = X + W(:, :, tau) * circshift(H,[0,tau-1]);
+end
+
+% undo zer0padding
+X = X(:,(L+1):(end-L)); 
+
+
